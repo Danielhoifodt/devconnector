049bb70e673ac3dd7c8c8e63b37fe4c906fa16ab
Danielhoifodt <40982934+Danielhoifodt@users.noreply.github.com>
2019-03-18 21:06:31 +0100

    Authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 // Load user model
 
@@ -70,7 +71,13 @@ router.post('/login', (req, res) => {
                     }
 
                 })
-        })
-})
+        });
+
+        
+});
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json(req.user);
+});
 
 module.exports = router;
